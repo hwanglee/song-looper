ace60f9a64e0e188852e665c89f2b6b2ee3747a1
Hwang Lee <hwangseung@gmail.com>
2022-04-10 17:40:46 -0400

    create time input component

---
+++ src/components/LoopTimeInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <n-input-group>
+    <n-input
+      size="large"
+      default-value="0:00"
+      @change="(value) => $emit('change', value)"
+    />
+    <n-button :focusable="false" size="large">Now</n-button>
+  </n-input-group>
+</template>
+
+<script setup lang="ts">
+import { NButton, NInput, NInputGroup } from "naive-ui";
+
+const emit = defineEmits<{
+  (e: "change", value: string): void;
+}>();
+</script>
