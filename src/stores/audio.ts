742728370e686ec436b9cd3b559f6fa706ae53f1
Hwang Lee <hwangseung@gmail.com>
2022-04-10 22:44:30 -0400

    add logs to audio store

--- src/stores/audio.ts
+++ src/stores/audio.ts
@@ -12,15 +12,19 @@ export const useAudioStore = defineStore({
   actions: {
     setIsPlaying(isPlaying: boolean) {
       this.isPlaying = isPlaying;
+      console.log(`set is playing: ${this.isPlaying}`);
     },
     setPlaybackRate(rate: number) {
       this.playbackRate = rate;
+      console.log(`set playback rate: ${this.playbackRate}`);
     },
     setCurrentTime(time: number) {
       this.currentTime = time;
+      console.log(`set current time: ${this.currentTime}`);
     },
     setIsLooping(isLooping: boolean) {
       this.isLooping = isLooping;
+      console.log(`set is looping: ${this.isLooping}`);
     },
   },
 });
