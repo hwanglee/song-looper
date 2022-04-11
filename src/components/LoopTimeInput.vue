a5caa05b8f4746713635143745494caec3206044
Hwang Lee <hwangseung@gmail.com>
2022-04-10 23:00:25 -0400

    format input as time

--- src/components/LoopTimeInput.vue
+++ src/components/LoopTimeInput.vue
@@ -2,8 +2,9 @@
   <n-input-group>
     <n-input
       size="large"
-      default-value="0:00"
-      @change="(value) => $emit('change', value)"
+      :value="inputValue"
+      @change="onChange"
+      @input="onInput"
     />
     <n-button :focusable="false" size="large">Now</n-button>
   </n-input-group>
@@ -11,8 +12,26 @@
 
 <script setup lang="ts">
 import { NButton, NInput, NInputGroup } from "naive-ui";
+import { ref } from "vue";
+
+const defaultTime = "0:00";
+const inputValue = ref(defaultTime);
+const currentTime = ref(defaultTime);
+const prevTime = ref(defaultTime);
 
 const emit = defineEmits<{
   (e: "change", value: string): void;
 }>();
+
+const onInput = (value: string) => {
+  inputValue.value = value;
+};
+
+const onChange = (value: string) => {
+  const formatted = value.formatAsTime();
+  prevTime.value = currentTime.value;
+  currentTime.value = formatted ?? prevTime.value;
+  inputValue.value = currentTime.value;
+  emit("change", inputValue.value);
+};
 </script>
