9604f6a7c62a4a6d8e8b901e04f0ac4aa2a213b8
Hwang Lee <hwangseung@gmail.com>
2022-04-11 23:45:27 -0400

    update test

--- src/extensions/tests/string-extensions.test.ts
+++ src/extensions/tests/string-extensions.test.ts
@@ -1,6 +1,12 @@
 import "../string-extensions";
 import "ts-jest";
 
-test("format '0:03' as time", () => {
-  expect("0:03".formatAsTime()).toBe("0:03");
+describe("format '0:03'", () => {
+  test("as time", () => {
+    expect("0:03".formatAsTime()).toBe("0:03");
+  });
+
+  test("as seconds", () => {
+    expect("0:03".convertToSeconds()).toBe(3);
+  });
 });
