4e02c9852a0df831fca7572ffc9c232e135ec597
Hwang Lee <hwangseung@gmail.com>
2022-04-11 23:49:47 -0400

    add more tests

--- src/extensions/tests/string-extensions.test.ts
+++ src/extensions/tests/string-extensions.test.ts
@@ -10,3 +10,23 @@ describe("format '0:03'", () => {
     expect("0:03".convertToSeconds()).toBe(3);
   });
 });
+
+describe("format '120", () => {
+  test("as time", () => {
+    expect("120".formatAsTime()).toBe("2:00");
+  });
+
+  test("as seconds", () => {
+    expect("120".convertToSeconds()).toBe(120);
+  });
+});
+
+describe("format 'asd", () => {
+  test("as time", () => {
+    expect("asd".formatAsTime()).toBe(undefined);
+  });
+
+  test("as seconds", () => {
+    expect("asd".convertToSeconds()).toBe(undefined);
+  });
+});
